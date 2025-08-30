0139ad9e31c70ef9771c4baad3087106a4c99aa3
Amit R Verma <amit@amitrverma.com>
2025-08-30 11:15:51 +0530

    env variable naming

--- src/app/_components/PostHogInit.tsx
+++ src/app/_components/PostHogInit.tsx
@@ -6,8 +6,8 @@ import posthog from "posthog-js";
 const PostHogInit = () => {
   useEffect(() => {
     if (typeof window !== "undefined") {
-      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
+      posthog.init(process.env.POSTHOG_KEY!, {
+        api_host: process.env.POSTHOG_HOST || "https://app.posthog.com",
         persistence: "localStorage+cookie", // best for logged-in flows
         defaults: '2025-05-24',
         person_profiles: "identified_only", // only when user logs in
